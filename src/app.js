278cf7445bf828a4045e8d1b93b883f60e41f63a
veda <thisisillusion@gmail.com>
2019-06-09 19:28:06 -0400

    finish section 53

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-import { add, square } from './utils';
-import { isAdult, canDrink } from './person';
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("app.js is running");
-
-console.log(square(4));
-console.log(add(2, 2));
-
-console.log(-1, isAdult(-1));
-console.log(95, isAdult(95));
-
-console.log(21, canDrink(21));
-console.log(35, canDrink(35));
-console.log(10, canDrink(10));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
