67f656862f013d0eb9bb437bec894d49a6a81584
Laura Bhayani <lbhayani@sentryone.com>
2019-08-19 13:05:45 -0400

    catch up to vid 65

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,14 @@ module.exports = {
     filename: "bundle.js",
   },
   module: {
-    rules:[{
+    rules: [{
       loader: "babel-loader",
       test: /\.js$/,
       exclude: /node_module/
+    },
+    {
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"]
     }]
   },
   devtool: "cheap-module-eval-source-map",
